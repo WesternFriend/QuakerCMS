bdd60ca2afeb4c52da282f434aa51af05e6ee374
Brylie Christopher Oxley <brylie@protonmail.com>
2025-07-02 21:28:44 +0300

    Refactor dev.py settings to use environment variable for SECRET_KEY and restrict ALLOWED_HOSTS

--- src/core/settings/dev.py
+++ src/core/settings/dev.py
@@ -1,13 +1,17 @@
 from .base import *
+import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-_odb62l=_u11+bj4a#@7g(hwhsik3e3_c5lti^g%1(h3)!!r1_"
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-_odb62l=_u11+bj4a#@7g(hwhsik3e3_c5lti^g%1(h3)!!r1_",
+)
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
